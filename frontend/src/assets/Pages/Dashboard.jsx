44721a7dd6e41c48930a3939b34a88afe76ba544
Aidan Schooley <schooleyaidan7@gmail.com>
2025-12-02 09:45:38 -0500

    frontend files and updates

---
+++ frontend/src/assets/Pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideMenu from '../Components/SideMenu.jsx'
+function Dashboard() {
+  return <div>
+    <h1>Dashboard Page</h1>
+    <h3>Welcome to the Dashboard!</h3>
+    <SideMenu />
+  </div>
+}
+
+export default Dashboard
