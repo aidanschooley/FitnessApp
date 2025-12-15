2e197b7f882eed3db3aff8ac848d3173963d4457
isaac-gasparri-27 <isaac.gasparri26@houghton.edu>
2025-12-15 15:07:55 -0500

    creating goals functionality

--- frontend/src/assets/Pages/goal.jsx
+++ frontend/src/assets/Pages/goal.jsx
@@ -1,13 +1,16 @@
 import SideMenu from "../components/sideMenu.jsx";
 import Login from "./Login.jsx";
+import CreateGoal from "../components/CreateGoal.jsx";
+
 function Goals() {
     const user = localStorage.getItem('user'); // Retrieve user data from local strorage
     if (user){
         return (
             <div>
                 <SideMenu />
-                <h1>Goals Page</h1> 
+                <h1>Goals Page</h1>
                 <p>This is where your goals will be displayed.</p>
+                <CreateGoal />
             </div>
         );
     }else{
